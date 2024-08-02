8944f471b11383a93efbcf48fe67f3cf140c698b
Nacho-Meter-Stick <138950899+Nacho-Meter-Stick@users.noreply.github.com>
2024-08-02 11:46:02 -0600

    Update GhostCube.py

--- solver/solver4/GhostCube.py
+++ solver/solver4/GhostCube.py
@@ -87,70 +87,6 @@ def GhostCube_from_cube2ds(cube2ds):
         if set(edge2) == {3, 5}: GhostCube[npind2(0, -1, -1)] = bitmask((edge3[edge2.index(3)], edge3[edge2.index(5)]))
         if set(edge2) == {4, 5}: GhostCube[npind2(-1, 0, -1)] = bitmask((edge3[edge2.index(4)], edge3[edge2.index(5)]))
     return GhostCube
-def update_GhostCube_with_move(GhostCube, move):
-                             # U  R  F  L  B  D
-    UpCornerTable = np.array([[9, 4, 1, 2, 3, 9],  # U
-                              [2, 9, 5, 9, 0, 4],  # R
-                              [3, 0, 9, 5, 9, 1],  # F
-                              [4, 9, 0, 9, 5, 2],  # L
-                              [1, 5, 9, 0, 9, 3],  # B
-                              [9, 2, 3, 4, 1, 9]]) # D
-                               # U  R  F  L  B  D
-    DownCornerTable = np.array([[9, 2, 3, 4, 1, 9],  # U
-                                [4, 9, 0, 9, 5, 2],  # R
-                                [1, 5, 9, 0, 9, 3],  # F
-                                [2, 9, 5, 9, 0, 4],  # L
-                                [3, 0, 9, 5, 9, 1],  # B
-                                [9, 4, 1, 2, 3, 9]]) # D
-    next_faces = [[[9, 2, 3, 4, 1, 9],
-                    [4, 9, 0, 9, 5, 2],
-                    [1, 5, 9, 0, 9, 3],
-                    [2, 9, 5, 9, 0, 4],
-                    [3, 0, 9, 5, 9, 1],
-                    [9, 4, 1, 2, 3, 9]],
-
-                    [[9, 3, 4, 1, 2, 9],
-                    [5, 9, 4, 9, 2, 0],
-                    [5, 3, 9, 1, 9, 0],
-                    [5, 9, 4, 9, 2, 0],
-                    [5, 3, 9, 1, 9, 0],
-                    [9, 3, 4, 1, 2, 9]],
-
-                    [[9, 4, 1, 2, 3, 9],
-                    [2, 9, 5, 9, 0, 4],
-                    [3, 0, 9, 5, 9, 1],
-                    [4, 9, 0, 9, 5, 2],
-                    [1, 5, 9, 0, 9, 3],
-                    [9, 2, 3, 4, 1, 9]]]
-    count = {"'": 3, "2": 2}.get(move[-1], 1)
-    npind = lambda x, y, z: (x+1, y+1, z+1)
-    # Edges
-    for x, y, z in ((-1, 0, 1), (1, 0, 1), (0, -1, 1), (0, 1, 1),
-                    (-1, -1, 0), (-1, 1, 0), (1, -1, 0), (1, 1, 0),
-                    (-1, 0, -1), (1, 0, -1), (0, -1, -1), (0, 1, -1)):
-        a0 = GhostCube[npind(x, y, z)]
-        b1 = a0>>3
-        b2 = a0&7
-        if b1 == move[0]: GhostCube[npind(x, y, z)] = (b1<<3)|next_faces[count-1][move[0]][b2]
-        if b2 == move[0]: GhostCube[npind(x, y, z)] = (next_faces[count-1][move[0]][b1]<<3)|b2
-    # Up corners
-    for x, y, z in ((-1, -1, 1), (-1, 1, 1), (1, -1, 1), (1, 1, 1)):
-        a0 = GhostCube[npind(x, y, z)]
-        b1 = a0>>3
-        b2 = a0&7
-        b3 = UpCornerTable[b1][b2]
-        if b1 == move[0]: GhostCube[npind(x, y, z)] = (b1<<3)|next_faces[count-1][move[0]][b2]
-        if b2 == move[0]: GhostCube[npind(x, y, z)] = (next_faces[count-1][move[0]][b1]<<3)|b2
-        if b3 == move[0]: GhostCube[npind(x, y, z)] = (next_faces[count-1][move[0]][b1]<<3)|next_faces[count-1][move[0]][b2]
-    # Down corners
-    for x, y, z in ((-1, -1, -1), (-1, 1, -1), (1, -1, -1), (1, 1, -1)):
-        a0 = GhostCube[npind(x, y, z)]
-        b1 = a0>>3
-        b2 = a0&7
-        b3 = DownCornerTable[b1][b2]
-        if b1 == move[0]: GhostCube[npind(x, y, z)] = (b1<<3)|next_faces[count-1][move[0]][b2]
-        if b2 == move[0]: GhostCube[npind(x, y, z)] = (next_faces[count-1][move[0]][b1]<<3)|b2
-        if b3 == move[0]: GhostCube[npind(x, y, z)] = (next_faces[count-1][move[0]][b1]<<3)|next_faces[count-1][move[0]][b2]
 '''
 cycles = [[1, 2, 3, 4],
         [2, 0, 4, 5],
@@ -165,3 +101,82 @@ for count in (1, 2, 3):
             next_face[count-1][move][face] = cycles[move][(cycles[move].index(face)+count)%4]
 print(next_face)
 '''
+next_faces = [[[9, 2, 3, 4, 1, 9],
+                [4, 9, 0, 9, 5, 2],
+                [1, 5, 9, 0, 9, 3],
+                [2, 9, 5, 9, 0, 4],
+                [3, 0, 9, 5, 9, 1],
+                [9, 4, 1, 2, 3, 9]],
+
+                [[9, 3, 4, 1, 2, 9],
+                [5, 9, 4, 9, 2, 0],
+                [5, 3, 9, 1, 9, 0],
+                [5, 9, 4, 9, 2, 0],
+                [5, 3, 9, 1, 9, 0],
+                [9, 3, 4, 1, 2, 9]],
+
+                [[9, 4, 1, 2, 3, 9],
+                [2, 9, 5, 9, 0, 4],
+                [3, 0, 9, 5, 9, 1],
+                [4, 9, 0, 9, 5, 2],
+                [1, 5, 9, 0, 9, 3],
+                [9, 2, 3, 4, 1, 9]]]
+def Down_corner_after_moves(moves: list[tuple[int, int]], bitmasked_tuple: np.uint8):
+                               # U  R  F  L  B  D
+    DownCornerTable = np.array([[9, 2, 3, 4, 1, 9],  # U
+                                [4, 9, 0, 9, 5, 2],  # R
+                                [1, 5, 9, 0, 9, 3],  # F
+                                [2, 9, 5, 9, 0, 4],  # L
+                                [3, 0, 9, 5, 9, 1],  # B
+                                [9, 4, 1, 2, 3, 9]]) # D
+    b1 = bitmasked_tuple>>3
+    b2 = bitmasked_tuple&7
+    b3 = DownCornerTable[b1][b2]
+    for move in moves:
+        mv = move[0]
+        count = move[1]
+        if b1 == mv: 
+            b2 = next_faces[count-1][mv][b2]
+            b3 = next_faces[count-1][mv][b3]
+        if b2 == mv: 
+            b1 = next_faces[count-1][mv][b1]
+            b3 = next_faces[count-1][mv][b3]
+        if b3 == mv: 
+            b1 = next_faces[count-1][mv][b1]
+            b2 = next_faces[count-1][mv][b2]
+    return (b1<<3)|b2
+def Up_corner_after_moves(moves: list[tuple[int, int]], bitmasked_tuple: np.uint8):
+                             # U  R  F  L  B  D
+    UpCornerTable = np.array([[9, 4, 1, 2, 3, 9],  # U
+                              [2, 9, 5, 9, 0, 4],  # R
+                              [3, 0, 9, 5, 9, 1],  # F
+                              [4, 9, 0, 9, 5, 2],  # L
+                              [1, 5, 9, 0, 9, 3],  # B
+                              [9, 2, 3, 4, 1, 9]]) # D
+    b1 = bitmasked_tuple>>3
+    b2 = bitmasked_tuple&7
+    b3 = UpCornerTable[b1][b2]
+    for move in moves:
+        mv = move[0]
+        count = move[1]
+        if b1 == mv: 
+            b2 = next_faces[count-1][mv][b2]
+            b3 = next_faces[count-1][mv][b3]
+        if b2 == mv: 
+            b1 = next_faces[count-1][mv][b1]
+            b3 = next_faces[count-1][mv][b3]
+        if b3 == mv: 
+            b1 = next_faces[count-1][mv][b1]
+            b2 = next_faces[count-1][mv][b2]
+    return (b1<<3)|b2
+def Edge_after_moves(moves: list[tuple[int, int]], bitmasked_tuple: np.uint8):
+    b1 = bitmasked_tuple>>3
+    b2 = bitmasked_tuple&7
+    for move in moves:
+        mv = move[0]
+        count = move[1]
+        if b1 == mv: 
+            b2 = next_faces[count-1][mv][b2]
+        if b2 == mv: 
+            b1 = next_faces[count-1][mv][b1]
+    return (b1<<3)|b2
