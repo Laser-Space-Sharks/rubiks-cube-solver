5478513a9abbac967f86ed91a28db2cb79f67b83
CodingKraken <tristaneggenberger204@gmail.com>
2024-08-15 22:27:21 -0600

    Replace tabs with space-expanded tabs

--- solver/solverC/src/solver.h
+++ solver/solverC/src/solver.h
@@ -8,31 +8,31 @@
 #include <stdio.h>
 
 typedef enum : uint8_t {
-	FACE_U = 0,
-	FACE_R = 1,
-	FACE_F = 2,
-	FACE_L = 3,
-	FACE_B = 4,
-	FACE_D = 5,
-	NUM_FACES,
+    FACE_U = 0,
+    FACE_R = 1,
+    FACE_F = 2,
+    FACE_L = 3,
+    FACE_B = 4,
+    FACE_D = 5,
+    NUM_FACES,
     FACE_NULL
 } face_e;
 
 typedef enum : char {
-	CHAR_U = 'U',
-	CHAR_R = 'R',
-	CHAR_F = 'F',
-	CHAR_L = 'L',
-	CHAR_B = 'B',
-	CHAR_D = 'D'
+    CHAR_U = 'U',
+    CHAR_R = 'R',
+    CHAR_F = 'F',
+    CHAR_L = 'L',
+    CHAR_B = 'B',
+    CHAR_D = 'D'
 } char_e;
 
 typedef enum : uint8_t {
-	SIDE_U = 0,
-	SIDE_R = 1,
-	SIDE_D = 2,
-	SIDE_L = 3,
-	NUM_SIDES
+    SIDE_U = 0,
+    SIDE_R = 1,
+    SIDE_D = 2,
+    SIDE_L = 3,
+    NUM_SIDES
 } side_e;
 
 /* Each element of a cube_s is a bitfield representing the state of a cube face,
@@ -56,22 +56,20 @@ typedef enum : uint8_t {
  */
 
 typedef struct {
-	uint64_t state[NUM_FACES];
+    uint64_t state[NUM_FACES];
 } cube_s;
 
 // fast rol instruction that simplifies to rolq (on x86 and rol on arm64)
 static inline uint64_t rolq(uint64_t n, uint8_t c) {
-	const uint64_t mask = CHAR_BIT*sizeof(n) - 1;
+    const uint64_t mask = CHAR_BIT*sizeof(n) - 1;
 
-	c &= mask;
-	return (n << c) | (n >> (-c & mask));
+    c &= mask;
+    return (n << c) | (n >> (-c & mask));
 }
 
 // takes positive mod of integer parameter with unsigned divisor
 static inline uint64_t positive_mod(int64_t n, uint64_t m) {
-	return (n % m + m) % m;
+    return (n % m + m) % m;
 }
-
-
 
 #endif
