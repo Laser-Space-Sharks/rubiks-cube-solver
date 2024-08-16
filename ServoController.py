adf170a6d061f9ffcf9f138237cf7e4605954a06
Crimson412 <139505390+Crimson412@users.noreply.github.com>
2024-08-15 19:16:06 -0600

    Update ServoController.py
    bad

--- ServoController.py
+++ ServoController.py
@@ -19,18 +19,46 @@ GPIO.setup(38, GPIO.OUT)
 
 #Note: First is the pin, 50 is 50hz for servo. Direction is relative to the camera
 # Sets up 8 variables for the servos
-servoL = GPIO.PWM(11,50)
 servoR = GPIO.PWM(12,50)
+servoL = GPIO.PWM(11,50)
 servoU = GPIO.PWM(13,50)
 servoD = GPIO.PWM(15,50)
 #Toggle Servos that engage and disengage the rotating servos
-toggleL = GPIO.PWM(35,50)
 toggleR = GPIO.PWM(36,50)
+toggleL = GPIO.PWM(35,50)
 toggleU = GPIO.PWM(37,50)
 toggleD = GPIO.PWM(38,50)
-
-referenceList[servoL, servoR, servoU, servoD, toggleL, toggleR, toggleU, toggleD][0*8]
 #PWM with pulse of 0 means pulse off
+
+servoList = [servoR, servoL, servoU, servoD, toggleR, toggleL, toggleU, toggleD]
+referenceList = [2,2,2,2,10,2,10,2]
+#Called to set servos into default state
+for i in range(8):
+    servoList[i].ChangeDutyCycle(referenceList[i])
+
+def servo_single_turn(servoIndex, b_clockwise):
+    if (b_clockwise):
+        if (referenceList[servoIndex] == 12):
+            print("bad")
+        else:
+            servoList[servoIndex].ChangeDutyCycle(referenceList[servoIndex]referenceList[servoIndex]+5)
+            referenceList[servoIndex] += 5
+    else:
+        if (referenceList[servoIndex] == 2):
+            print("bad")
+        else:
+            servoList[servoIndex].ChangeDutyCycle(referenceList[servoIndex]-5)
+            referenceList[servoIndex] -= 5
+
+def servo_full_turn(servoIndex):
+    if (referenceList[servoIndex] == 7):
+        print("bad")
+    elif (referenceList[0] == 2):
+        servoList[servoIndex].ChangeDutyCycle(referenceList[servoIndex]+10)
+        referenceList[0] += 10
+    else:
+        servoList[servoIndex].ChangeDutyCycle(referenceList[servoIndex]-10)
+        referenceList[0] -= 10
 
 #The String that is fed is made into an array split between each space
 commandList = testString.split()
@@ -40,57 +68,57 @@ for cmd in commandList:
     for i in SyncedCmdList:
         match str(i): 	
             case "R":
-                servoR.ChangeDutyCycle(duty)
+                servo_single_turn(0, True)
             case "R'":
-                print("Two")
+                servo_single_turn(0, False)
             case "R2":
-                print("l")
+                servo_full_turn(0)
             case "L":
-                print("Three")
+                servo_single_turn(1, True)
             case "L'":
-                print("yo")
+                servo_single_turn(1, False)
             case "L2":
-                print("l")
+                servo_full_turn(1)
             case "U":
-                print("l")
+                servo_single_turn(2, True)
             case "U'":
-                print("l")
+                servo_single_turn(2, False)
             case "U2":
-                print("l")
+                servo_full_turn(2)
             case "D":
-                print("l")
+                servo_single_turn(3, True)
             case "D'":
-                print("l")
+                servo_single_turn(3, False)
             case "D2":
-                print("l")
+                servo_full_turn(3)
             case "F":
-                print("l")
+                print("bad")
             case "F'":
-                print("l")
+                print("bad")
             case "F2":
-                print("l")
+                print("bad")
             case "B":
-                print("l")
+                print("bad")
             case "B'":
-                print("l")
+                print("bad")
             case "B2":
-                print("l")
+                print("bad")
             case "R:E":
-                print("l")
+                print("bad")
             case "R:E'":
-                print("l")
+                print("bad")
             case "L:E":
-                print("l")
+                print("bad")
             case "L:E'":
-                print("l")
+                print("bad")
             case "U:E":
-                print("l")
+                print("bad")
             case "U:E'":
-                print("l")
+                print("bad")
             case "D:E":
-                print("l")
+                print("bad")
             case "D:E'":
-                print("l")
+                print("bad")
             case _:
                 print("Invalid Command!" + str(i))
     time.sleep()
