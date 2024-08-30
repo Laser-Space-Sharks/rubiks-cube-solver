db4ac51954e832b9f44667b461b55140cec1a6ef
Nacho-Meter-Stick <138950899+Nacho-Meter-Stick@users.noreply.github.com>
2024-08-29 18:47:47 -0600

    Update login_speedrunner.py

--- login_speedrunner.py
+++ login_speedrunner.py
@@ -1,7 +1,9 @@
 import os
+import time
 with open("scan.txt") as file:
     for line in file.readlines():
         pattern = "Nmap scan report for "
+        time.sleep(0.2)
         if line[:len(pattern)] == pattern:
             ip_address = line[len(pattern):]
             command = f"ssh pi@{ip_address}"
