89e9dcc4a8b44b7ebae5359c0e010f7df224c3b1
kraken <tristaneggenberger204@gmail.com>
2024-09-28 09:35:33 -0600

    Add user input option for displaying the result of an algorithm

--- solver/solverC/src/main.c
+++ solver/solverC/src/main.c
@@ -1,10 +1,7 @@
-#include <bits/time.h>
-#include <time.h>
 #include <unistd.h>
 
 #include "main.h"
 #include "cube.h"
-#include "solver.h"
 #include "move.h"
 
 int main(int argc, char *argv[]) {
@@ -18,7 +15,11 @@ int main(int argc, char *argv[]) {
     init_move_list(&moves, 10);
 
     printf("Algorithm to apply:\n");
-    move_list_from_move_str(&moves, "L2 R2 U2 D2 F2 B2");
+    if (argc > 1) {
+        move_list_from_move_str(&moves, argv[1]);
+    } else {
+        move_list_from_move_str(&moves, "U R2 F B R B2 R U2 L B2 R U' D' R2 F R' L B2 U2 F2");
+    }
     print_move_list(moves);
     apply_move_list(&cube, &moves);
     printf("\n");
