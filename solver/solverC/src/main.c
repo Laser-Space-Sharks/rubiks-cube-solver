865da82d959f5f8a648ff9bf13772b3653e6b9de
kraken <tristaneggenberger204@gmail.com>
2024-12-16 21:55:16 -0700

    remove debug printfs and fix imports

--- solver/solverC/src/main.c
+++ solver/solverC/src/main.c
@@ -1,39 +1,40 @@
 #include "main.h"
+#include "alg.h"
 #include "shift_cube.h"
 #include "cube18B.h"
-#include "alg.h"
 #include "solver.h"
 
 #include <time.h>
 
 int main(int argc, char *argv[]) {
-    printf("Got to line 10 of main.c");
+
     init_move_bitrolls();
+
     printf("Got to line 12 of main.c");
 
     shift_cube_s cube = SOLVED_SHIFTCUBE;
 
-    //cube_table_s *f2l_table = generate_f2l_table("../../ALGORITHMS/FULL_F2L_ALGORITHMS.txt");
-    //cube_table_s *last_layer_table = generate_last_layer_table("../../ALGORITHMS/FULL_1LLL_ALGORITHMS.txt");
+//    cube_table_s *f2l_table = generate_f2l_table("../../ALGORITHMS/FULL_F2L_ALGORITHMS.txt");
+//    cube_table_s *last_layer_table = generate_last_layer_table("../../ALGORITHMS/FULL_1LLL_ALGORITHMS.txt");
 
-    //init_solver();
+    init_solver();
 
     alg_s *alg = NULL;
     #define NUM_TESTS 9
-    char *scrambles[] = {
-        "F D' R2 D' L' F L B' U R D' R F' U2 F D R U' F' D2 L U' R2 B' U2",
-        "L' B R2 F2 L' B L' D' F' L' D2 R' B' R F R' F R F U L B L U' R'",
-        "D F L U B' U' L2 B' L' B' U' R' D F' D' L2 D F L U L' D2 L U L'",
-        "B2 D R' F' R2 B' D2 L2 D B2 D L' F D2 L2 D L' F' R2 U L' D' F U B'",
-        "R' D F L' D' R' D F2 R' F' R' B' R F2 R B' U F' L' D B2 L' D L' F",
-        "L' B D F' L' B D2 B L' B' D L' U B L D R' B2 R D2 R U L D' B",
-        "D B' L' D F' R' D L F2 U F D' L F' L' F' D' L U' B D R B' U2 F",
-        "L2 D R2 F D R2 U2 R' F' R' F' L F D R B' U R' U F' D B' R' B R'",
-        "F2 U L' U R' U L U B' L F D' F' U' R' D F2 R B' L D2 B' L' F' L'"
-    };
+//    char *scrambles[] = {
+//        "F D' R2 D' L' F L B' U R D' R F' U2 F D R U' F' D2 L U' R2 B' U2",
+//        "L' B R2 F2 L' B L' D' F' L' D2 R' B' R F R' F R F U L B L U' R'",
+//        "D F L U B' U' L2 B' L' B' U' R' D F' D' L2 D F L U L' D2 L U L'",
+//        "B2 D R' F' R2 B' D2 L2 D B2 D L' F D2 L2 D L' F' R2 U L' D' F U B'",
+//        "R' D F L' D' R' D F2 R' F' R' B' R F2 R B' U F' L' D B2 L' D L' F",
+//        "L' B D F' L' B D2 B L' B' D L' U B L D R' B2 R D2 R U L D' B",
+//        "D B' L' D F' R' D L F2 U F D' L F' L' F' D' L U' B D R B' U2 F",
+//        "L2 D R2 F D R2 U2 R' F' R' F' L F D R B' U R' U F' D B' R' B R'",
+//        "F2 U L' U R' U L U B' L F D' F' U' R' D F2 R B' L D2 B' L' F' L'"
+//    };
 
     if (argc > 1) {
-        alg = alg_from_alg_str(argv[1]);
+//        alg = alg_from_alg_str(argv[1]);
     } else {
         /*
         printf("Performing tests\n");
@@ -59,14 +60,11 @@ int main(int argc, char *argv[]) {
         printf("Average solve length: %f\n", sum / NUM_TESTS);
         */
 
-        printf("Got to line 62 of main.c");
         alg = alg_from_alg_str("F D' R2 D' L' F L B' U R D' R F' U2 F D R U' F' D2 L U' R2 B' U2");
-        printf("Got to line 64 of main.c");
         init_all_tables_in_cube18Bc();
-        printf("Got to line 66 of main.c");
         shift_cube_s shiftcube = SOLVED_SHIFTCUBE;
         cube18B_s cube18B = SOLVED_CUBE18B;
-        cube18B_xcross_s xcross = SOLVED_CUBE18B_XCROSS;
+//        cube18B_xcross_s xcross = SOLVED_CUBE18B_XCROSS;
         print_cube_line_colors(shiftcube);
         print_cube18B(&cube18B);
         printf("FACE_NULL is %d\n", FACE_NULL);
