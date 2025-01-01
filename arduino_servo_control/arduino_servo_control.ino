78ac8e8256223707304ddc7bb528b6a324297686
Crimson <lorenzo.marez412@gmail.com>
2025-01-01 15:06:14 -0700

    added updated servo code with times

--- arduino_servo_control/arduino_servo_control.ino
+++ arduino_servo_control/arduino_servo_control.ino
@@ -1,110 +1,109 @@
 #include <Servo.h> 
-Servo servoR, servoL, servoU, servoD, servoRE, servoLE, servoUE, servoDE;
+//Subscript indicates Right, Left, Up, Down for the turning servos, and East, West, North, and South for the engage servos all relative to the camera
+Servo servoR, servoL, servoU, servoD, servoE, servoW, servoN, servoS;
 //https://docs.arduino.cc/learn/electronics/servo-motors
 
 #define timeDelay 5000 //in miliseconds
-#define engageAngle 5000
+#define engageAngle 144 //max angle that engage servos go to
+#define halfTurn 500 //in Milliseconds
+#define quarterTurn 350 //in Milliseconds
+#define engageTime 375 //in Milliseconds
 
-struct command{
-  int length;
-  Servo servo[];
-  int angle[];
-};
 
-//0-180 for all servos is clockwise, U, R, L, D | 180-0 is U', R', L', D'
-//EngageAngle-0 For Right+UP engage servos is Engage (RE or UE), 0-EngageAngle is Disengage (RE' or UE')
-//0-EngageAngle for Left+Down engage servos is Engage (LE or DE), EngageAngle-0 is Disengage (LE' or DE')
+//0-180 for all servos is clockwise | 180-0 is counter clockwise
+//0 For East+North servos is Engage (capital char) | engageAngle is Disengage (lowercase char)
+//engageAngle For West+South servos is Engage (capital char) | 0 is Disengage (lowercase char)
 
-void execute(char str[], int len, int cmdAmnt){
-  command commands[cmdAmnt];
-  int start = 0, cmd = 0, simulCount = 0;
-  for(int i = 0; i < len; i++){
-    if(str[i] == ' ' || str[i] == '.'){
+void execute(char str[], int len){
+  int start = 0, angle;
+  for(int i = 0; i <= len; i++){
+    if(str[i] == ' ' || str[i] == '.' || i == len){
+      angle = (str[start+1] - '0')*90;
       switch (str[start]) {
-        case '1':
-          commands[cmd].servo[simulCount] = servoR;
+        case 'R':
+          servoR.write(angle);
           break;
-        case '2':
-          commands[cmd].servo[simulCount] = servoL;
+        case 'L':
+          servoL.write(angle);
           break;
-        case '3':
-          commands[cmd].servo[simulCount] = servoU;
+        case 'U':
+          servoU.write(angle);
           break;
-        case '4':
-          commands[cmd].servo[simulCount] = servoD;
+        case 'D':
+          servoD.write(angle);
           break;
-        case '5':
-          commands[cmd].servo[simulCount] = servoRE;
+        case 'E':
+          servoE.write(0);
           break;
-        case '6':
-          commands[cmd].servo[simulCount] = servoLE;
+        case 'e':
+          servoE.write(engageAngle);
           break;
-        case '7':
-          commands[cmd].servo[simulCount] = servoUE;
+        case 'W':
+          servoW.write(engageAngle);
           break;
-        case '8':
-          commands[cmd].servo[simulCount] = servoDE;
+        case 'w':
+          servoW.write(0);
           break;
-        start++;
-        int angleLength = i - start;
-        String angle;
-        for(int i = 0; i < angleLength; i++)
-          angle[i] = str[start+i];
-        angle[angleLength] = '\0';
-        commands[cmd].angle[simulCount] = angle.toInt();
-        if(str[i] == ' '){
-          commands[cmd].length = simulCount+1;
-          cmd++;
-          simulCount = 0;
-        }
-        else
-          simulCount++;
-        start = i+1;
+        case 'N':
+          servoN.write(0);
+          break;
+        case 'n':
+          servoN.write(engageAngle);
+          break;
+        case 'S':
+          servoS.write(engageAngle);
+          break;
+        case 's':
+          servoS.write(0);
+          break;
       }
+    if(str[i] == ' ' || i == len)
+      delay(timeDelay);
+    start = i+1;
     }
-  }
-  for(int i = 0; i < cmdAmnt; i++){
-    for(int j = 0; j < commands[i].length; j++)
-      commands[i].servo[j].write(commands[i].angle[j]);
-    delay(timeDelay);
   }
 }
 
 void setup() {
   //pwm, and normal digital pins all work for this
   servoR.attach(22); servoL.attach(23); servoU.attach(24); servoD.attach(25);
-  servoRE.attach(28); servoLE.attach(29); servoUE.attach(30); servoDE.attach(31);
+  servoE.attach(28); servoW.attach(29); servoN.attach(30); servoS.attach(31);
   Serial.begin(9600);
-  //set servos to default values
+  //set servos to default values | 0 for rotating servos, disengage for engage servos
   servoR.write(0); servoL.write(0); servoU.write(0); servoD.write(0);
-  servoRE.write(engageAngle); servoLE.write(0); servoUE.write(engageAngle); servoDE.write(0);
+  servoE.write(engageAngle); servoW.write(0); servoN.write(engageAngle); servoS.write(0);
+
+  char testStr[] = "R0.L0.U0.D0.e.w.n.s E.W.N.S"; //"R0.L0.U0.D0.e.w.n.s R2.L2.U2.D2 R1.L1.U1.D1 E.W.N.S e.w.n.s";//"R2.L2 R0.E R1.e";
+  int count = 0;
+  for(; testStr[count]; count++){}
+  execute(testStr, count);
 }
 
 void loop() {
-  //test string "U.RE'.LE' R2.L' RE.LE"
-  char str[] = "1180.290 30 490.5144.60";
-  int count, cmdAmnt = 0;
-  for(int i  = 0; str[i]; i++){
-    count = i;
-    if(str[i] == ' ')
-      cmdAmnt++;
-  }
-  execute(str, count, cmdAmnt);
-
-  /*char byte = 0;
+  //code to get search for string
+  /*then:
+  int count = 0;
+  for(; testStr[count]; count++){}
+  execute(testStr, count);
+  */
+  char byte = 0;
   Serial.readBytes(&byte, 1);
   Serial.print(byte);
   if (byte == 'R'){
-    servoR.write(180);
     servoL.write(0);
+    //servoL.writeMicroseconds(1280);
   }
   if (byte == 'L'){
-    servoR.write(0);
-    servoL.write(180);
+    servoL.write(144);
   }
   if (byte == 'O'){
-    servoR.write(90);
-    servoL.write(90);
-  }*/
-
+    servoL.writeMicroseconds(2000);
+  }
+  if (byte == 'e')
+  {
+    servoL.write(engageAngle);
+  }
+  if (byte == 'E'){
+    servoL.write(45);
+  }
 }
