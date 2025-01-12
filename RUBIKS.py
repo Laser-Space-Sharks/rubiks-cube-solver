2740b32beaa5f42d824e840103738d56b3557ba3
Crimson <lorenzo.marez412@gmail.com>
2025-01-12 13:36:27 -0700

    added gpio warning suppress to RUBIKS.py

--- RUBIKS.py
+++ RUBIKS.py
@@ -28,6 +28,7 @@
 
 import RPi.GPIO as GPIO # Import Raspberry Pi GPIO library
 
+GPIO.setwarnings(False) # Ignore warnings from GPIO
 GPIO.setmode(GPIO.BOARD) # Use physical pin numbering
 GPIO.setup(10, GPIO.IN, pull_up_down=GPIO.PUD_DOWN) # Set pin 10 to be an input pin and set initial value to be pulled low (off)
 
