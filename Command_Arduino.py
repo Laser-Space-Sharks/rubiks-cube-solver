69424e43e95013e90f8cf71dd3896cf5a0b8b428
Nacho_Meter_Stick <ckrich1013@gmail.com>
2025-01-12 13:25:14 -0700

    rewrote arduino push func

--- Command_Arduino.py
+++ Command_Arduino.py
@@ -3,5 +3,16 @@ import serial
 ARDUINO = serial.Serial(port='/dev/ttyACM0', baudrate=9600, timeout=.1)
 
 def push_robotNums_to_arduino(nums: list[int]):
-    bitstring = ''.join([str((num>>i)&1) for num in nums for i in range(12)])
-    ARDUINO.write(bytes([int(bitstring[i:i+8], 2) for i in range(0, len(bitstring), 8)]))
+    num_bits = len(nums)*12
+    num_bytes = (num_bits + 4)//8
+    total_num = 0
+    for num in nums:
+        for i in range(11,-1,-1):
+            total_num <<= 1
+            total_num |= ((num>>i)&1)
+    bytes = [0 for _ in range(num_bytes)]
+    for i in range(num_bits):
+        bytes[i//8] <<= 1
+        bytes[i//8] |= (total_num>>(num_bits-1-i))&1
+    if (num_bytes & 1 == 1): bytes[-1] <<= 4
+    ARDUINO.write(bytes, num_bytes)
