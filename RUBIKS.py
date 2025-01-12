7ca6da780cfb4fd19ac9455dcc9a912fb391f7bf
Nacho_Meter_Stick <ckrich1013@gmail.com>
2025-01-12 15:11:14 -0700

    hopefully

--- RUBIKS.py
+++ RUBIKS.py
@@ -16,6 +16,8 @@ GPIO.setwarnings(False) # Ignore warnings from GPIO
 GPIO.setmode(GPIO.BOARD) # Use physical pin numbering
 GPIO.setup(10, GPIO.IN, pull_up_down=GPIO.PUD_DOWN) # Set pin 10 to be an input pin and set initial value to be pulled low (off)
 
+print("Ready To Go!")
+
 while True:
     if GPIO.input(10) == GPIO.HIGH:
         print("Button was pushed!")
