60d6823b0b44a343362e6412f4a0fdf55b077961
aagowl <abigail.gatsch@gmail.com>
2025-01-12 12:52:53 -0700

    add more outline to main cube program

--- RUBIKS.py
+++ RUBIKS.py
@@ -10,8 +10,18 @@
 # ucolor = getCenterColor(captureImg(CUBE_IMG_FOLDER, up, False))
 # colorsArray = genColorsArray(fcolor, ucolor)
 # initialize cube Array
-# cubeArr = addFaceToCubeArray(scanFace(colorsArray, True, front), orientation, cubeArr)
-# addFaceToCubeArray(scanFace(colorsArray, True, up), orientation, cubeArr)
+# cubeArr = addFaceToCubeArray(scanFace(colorsArray, True, "front"), orientation, cubeArr)
+# addFaceToCubeArray(scanFace(colorsArray, True, "up"), orientation, cubeArr)
 # for i=2; i++; i<6:
 #   Move_to_faceN(i)
-#   addFaceToCubeArray(scanCube(colorsArray))
+#   cubeArr = addFaceToCubeArray(scanCube(colorsArray))
+#  shiftCubeArr = convertToShiftCube(cubeArr)
+# run(["./solver", 
+#    "-i", "shiftcube", 
+#     "-o", "servocode", 
+#     f"{shiftCubeArr[0]}", 
+#     f"{shiftCubeArr[1]}", 
+#     f"{shiftCubeArr[2]}", 
+#     f"{shiftCubeArr[3]}", 
+#     f"{shiftCubeArr[4]}", 
+#     f"{shiftCubeArr[5]}"])
