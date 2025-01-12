9e5c488e5e1fff90601d2e8f3814c3165d27a0f5
Nacho_Meter_Stick <ckrich1013@gmail.com>
2025-01-12 15:16:42 -0700

    hopefully

--- Command_Arduino.py
+++ Command_Arduino.py
@@ -1,6 +1,6 @@
 import serial
 
-ARDUINO = serial.Serial(port='/dev/ttyACM0', baudrate=9600, timeout=.1)
+ARDUINO = serial.Serial(port='/dev/ttyACM0', baudrate=9600, timeout=1)
 
 def push_robotNums_to_arduino(nums: list[int]):
     num_bits = len(nums)*12
@@ -14,4 +14,4 @@ def push_robotNums_to_arduino(nums: list[int]):
         bytes[i//8] <<= 1
         bytes[i//8] |= (total_num>>(num_bits-1-i))&1
     if (num_bytes & 1 == 1): bytes[-1] <<= 4
-    ARDUINO.write(bytes, num_bytes)
+    ARDUINO.write(''.join([chr(i) for i in bytes]))
