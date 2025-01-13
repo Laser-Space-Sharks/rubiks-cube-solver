d1c3cba9860ddad04994175f974e07dd534fd3b6
aagowl <abigail.gatsch@gmail.com>
2025-01-12 17:47:51 -0700

    add scanCube function

--- RUBIKS.py
+++ RUBIKS.py
@@ -11,6 +11,7 @@ from servoCoding.DataTypes import Orientation
 from image_processing import getCenterColor, CUBE_IMG_FOLDER,\
 captureImg, genColorsArray, addFaceToCubeScan, convertToShiftCube, scanFace,\
 errorDetection
+from Command_Arduino import push_robotStrs_to_arduino
 from subprocess import run 
 from numpy import zeros 
 
@@ -20,28 +21,31 @@ GPIO.setup(10, GPIO.IN, pull_up_down=GPIO.PUD_DOWN) # Set pin 10 to be an input 
 
 print("Ready To Go!")
 
+def scanCube():
+    # Start Scanning and initalize colors
+    O1: Orientation = Move_to_faceN(0)
+    ucolor = getCenterColor(captureImg(CUBE_IMG_FOLDER, "up", False))
+    O2: Orientation = Move_to_faceN(1)
+    fcolor = getCenterColor(captureImg(CUBE_IMG_FOLDER, "front", False))
+    colorsArray = genColorsArray(fcolor, ucolor)
+    cubeArr = zeros(shape=(6, 3, 3))
+    cubeArr = addFaceToCubeScan(scanFace(colorsArray, True, "front"), O2, cubeArr)
+    cubeArr = addFaceToCubeScan(scanFace(colorsArray, True, "up"), O1, cubeArr)
+    # Scan rest of cube and convert to shift cube
+    for i in range(2, 6):
+        O: Orientation = Move_to_faceN(i)
+        cubeArr = addFaceToCubeScan(scanFace(colorsArray), O, cubeArr)
+    if (not errorDetection(cubeArr)):
+        print("Cube scan failed!")
+        Move_to_default()
+        continue
+    return convertToShiftCube(cubeArr)
+
 while True:
     if GPIO.input(10) == GPIO.HIGH:
         print("Button was pushed!")
         # Start solving!
-        # Start Scanning and initalize colors
-        O1: Orientation = Move_to_faceN(0)
-        ucolor = getCenterColor(captureImg(CUBE_IMG_FOLDER, "up", False))
-        O2: Orientation = Move_to_faceN(1)
-        fcolor = getCenterColor(captureImg(CUBE_IMG_FOLDER, "front", False))
-        colorsArray = genColorsArray(fcolor, ucolor)
-        cubeArr = zeros(shape=(6, 3, 3))
-        cubeArr = addFaceToCubeScan(scanFace(colorsArray, True, "front"), O2, cubeArr)
-        cubeArr = addFaceToCubeScan(scanFace(colorsArray, True, "up"), O1, cubeArr)
-        # Scan rest of cube and convert to shift cube
-        for i in range(2, 6):
-            O: Orientation = Move_to_faceN(i)
-            cubeArr = addFaceToCubeScan(scanFace(colorsArray), O, cubeArr)
-        if (not errorDetection(cubeArr)):
-            print("Cube scan failed!")
-            Move_to_default()
-            continue
-        shiftCubeArr = convertToShiftCube(cubeArr)
+        shiftCubeArr = scanCube()
         # run solverc
         solverOut = run([
             "./solverc/shiftcube/solverpi", 
@@ -54,6 +58,7 @@ while True:
             f"{shiftCubeArr[4]}", 
             f"{shiftCubeArr[5]}"],
             check=True, stdout=PIPE).stdout
+        push_robotStrs_to_arduino(solverOut.split())
 
 
 
